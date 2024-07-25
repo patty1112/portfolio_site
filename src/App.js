08736e99db426d3933179cf2dbfce68117ec2a49
Patrick Maes <patrickmaes@mines.edu>
2024-07-25 11:32:10 -0600

    Initial Commit. Created the browser router, commented out since home layout is not complete. Created nav-bar with CSS styling, linking to future routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Layout from './Layout'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 
-function App() {
+
+const App = () => {
+  // const router = createBrowserRouter([
+  //     {
+  //       path: "/",
+  //       element: <Layout/>
+  //     }
+  // ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.StrictMode>
+      <header className="navbar">
+      <ul>
+          {<li><a href="/home" className="navbar-button">Home</a></li>}
+          {<li><a href="/about" className="navbar-button">About</a></li>}
+          {<li><a href="/projects" className="navbar-button">Projects</a></li>}
+        </ul>
       </header>
-    </div>
+    </React.StrictMode>
   );
 }
 
