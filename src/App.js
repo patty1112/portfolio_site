9004db7bda5dc8dc7071a50c7a1fd4722f5d26ff
Patrick Maes <patrickmaes@mines.edu>
2024-07-27 22:40:14 -0600

    Created Navbar with dropdown menu.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react'
-import Layout from './Layout'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
-
+import React from 'react';
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import { Link, Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  // const router = createBrowserRouter([
-  //     {
-  //       path: "/",
-  //       element: <Layout/>
-  //     }
-  // ]);
+
   return (
     <React.StrictMode>
-      <header className="navbar">
-      <ul>
-          {<li><a href="/home" className="navbar-button">Home</a></li>}
-          {<li><a href="/about" className="navbar-button">About</a></li>}
-          {<li><a href="/projects" className="navbar-button">Projects</a></li>}
-        </ul>
-      </header>
+       <div class="navbar">
+       <div class="dropdown">
+          <button class="dropbtn"> Projects
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div className='topnav-right'>
+          <a href="#home">About</a>
+          <a href="#news">Resume</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projets" element={<Projects />} />
+      </Routes>
     </React.StrictMode>
   );
 }
